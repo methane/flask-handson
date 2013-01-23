366f8f4846140d561c713e6c8b6237ebb1ddaaba
INADA Naoki <inada-n@klab.com>
2013-01-23 21:02:44 +0900

    モデルに __repr__ を追加

--- src/flaskr/flaskr/models.py
+++ src/flaskr/flaskr/models.py
@@ -1,11 +1,16 @@
+# coding: utf-8
 from flaskr import db
 from sqlalchemy import *
 
 
 class Entry(db.Model):
+    u"ブログエントリ"
     id = Column(Integer, primary_key=True)
     title = Column(Text)
     text = Column(Text)
+
+    def __repr__(self):
+        return "<Entry id={} title='{}'>".format(self.id, self.title)
 
 
 def init():
